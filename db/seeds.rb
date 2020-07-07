252e1d1f613025c9a4392c55bcaae12eb4589e33
Yanny Castrillon <yannyandres117@gmail.com>
2020-07-07 11:56:39 -0500

    Add gem to the gemfile. Add associations to our models. Modify seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  team = Team.create(
+    name: Faker::Artist.name,
+    description: Faker::TvShows::GameOfThrones.quote
+  )
+  3.times do
+    Player.create(first_name: Faker::Music::UmphreysMcgee.song,
+                  last_name: Faker::Lorem.sentence,
+                  position: "https//www.#{Faker::Lorem.word}.com",
+                  team_id: team.id)
+  end
+end
